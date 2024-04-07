33beabd53fa29eb365ac4aa486caa74514194f92
julien44830 <deniaudjulien0@gmail.com>
2024-04-08 00:28:10 +0200

    ajout du h1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Particules from "./compoments/Particules"
+import "./index.css";
 
 
 
@@ -6,6 +7,7 @@ function App() {
 
   return (
     <>
+  <h1>Julien Dev</h1>
   <Particules/>
     </>
   )
