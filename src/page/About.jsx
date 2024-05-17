9dc3afa990522eb703b4a7124177a00aa60afcf4
julien44830 <deniaudjulien0@gmail.com>
2024-05-17 08:02:45 +0200

    css About

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -1,11 +1,14 @@
 import { useLoaderData } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../styles/about.css";
 function About() {
     const kata = useLoaderData();
 
     return (
         <>
-            <h1>je suis MOI !!!</h1>
+            <NavLink to="/">
+                <h1 className="h1-code-wars">CodeWars et moi</h1>
+            </NavLink>
 
             <table>
                 <caption>
@@ -30,13 +33,22 @@ function About() {
                 ))}
                 <tfoot>
                     <tr>
-                        <td>------------------------------</td>
+                        <td></td>
+                        <td>...en attente de resolution de Kata...</td>
+                        <td></td>
                     </tr>
                 </tfoot>
             </table>
 
+            <section>
+                <p>donnée récupérée sur :</p>
+                <p>
+                    https://www.codewars.com/api/v1/users/julien44830/code-challenges/completed?page=page
+                </p>
+            <p>------------------------------</p>
             <p>*Heure local = heure GTM + 2h </p>
             <p>------------------------------</p>
+            </section>
         </>
     );
 }
