b1a384d3d8f86f89256fffcfab237a9a7a2788a8
julien44830 <deniaudjulien0@gmail.com>
2024-05-23 22:33:31 +0200

    ajout du projet H2ovrflow && modification de l'heure en heure locale composant codewars

---
+++ src/compoments/H2oveflow.jsx
@@ -0,0 +1,14 @@
+import "../styles/h2overflow.css";
+
+function H2overflow() {
+    return (
+        <div id="iframeContainer">
+            <iframe
+                id="iframeContent"
+                src="https://h2overflow-git-main-julien44830s-projects.vercel.app/"
+            ></iframe>
+        </div>
+    );
+}
+
+export default H2overflow;
--- src/page/Projet.jsx
+++ src/page/Projet.jsx
@@ -1,14 +1,15 @@
 import { NavLink } from "react-router-dom";
+import H2overflow from "../compoments/H2oveflow";
 
-
-function Projet(){
-
-return(
-    <NavLink to="/">
-
-    <h1>coucou projet</h1>
-    </NavLink>
-)
+function Projet() {
+    return (
+        <>
+            <NavLink to="/">
+                <h1>coucou projet</h1>
+            </NavLink>
+            <H2overflow />
+        </>
+    );
 }
 
-export default Projet
+export default Projet;
