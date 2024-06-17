c723a07060849a77a68606212b92489e6c315ec6
julien44830 <deniaudjulien0@gmail.com>
2024-06-17 21:27:08 +0200

    ajout de la video pour background codewars

--- src/page/CodeWars.jsx
+++ src/page/CodeWars.jsx
@@ -10,31 +10,57 @@ function CodeWars() {
                 <h1 className="h1-code-wars">CodeWars et moi</h1>
             </NavLink>
 
-            <table>
-                <caption>
-                    Nombres de kata realisé sur CodeWars : {kata.data.length}
-                </caption>
-                <thead>
-                    <tr>
-                        <th scope="col">realisé le</th>
-                        <th scope="col">Langage</th>
-                        <th scope="col">Nom du kata</th>
-                    </tr>
-                </thead>
-
-                {kata.data.map((e, index) => (
-                    <tbody key={index}>
+            <div className="container-table_codewars">
+                <table className="table-codewars">
+                    <caption>
+                        Nombres de kata realisé sur CodeWars :{" "}
+                        {kata.data.length}
+                    </caption>
+                    <thead>
                         <tr>
-                            <th>{new Date(e.completedAt).toLocaleDateString()} {new Date(e.completedAt).toLocaleTimeString()} </th>
-                            <th>{e.completedLanguages}</th>
-                            <th>{`=> ${e.name}`}</th>
+                            <th
+                                scope="col"
+                                className="th-codewars"
+                            >
+                                realisé le
+                            </th>
+                            <th
+                                scope="col"
+                                className="th-codewars"
+                            >
+                                Langage
+                            </th>
+                            <th
+                                scope="col"
+                                className="th-codewars"
+                            >
+                                Nom du kata
+                            </th>
                         </tr>
-                    </tbody>
-                ))}
-                <tfoot>
-                        ...en attente de resolution de Kata...
-                </tfoot>
-            </table>
+                    </thead>
+
+                    {kata.data.map((e, index) => (
+                        <tbody key={index}>
+                            <tr>
+                                <th className="th-codewars">
+                                    {new Date(
+                                        e.completedAt
+                                    ).toLocaleDateString()}
+                                    {" à "}
+                                    {new Date(
+                                        e.completedAt
+                                    ).toLocaleTimeString()}{" "}
+                                </th>
+                                <th className="th-codewars">
+                                    {e.completedLanguages}
+                                </th>
+                                <th className="th-codewars">{`=> ${e.name}`}</th>
+                            </tr>
+                        </tbody>
+                    ))}
+                    <tfoot>...en attente de resolution de Kata...</tfoot>
+                </table>
+            </div>
 
             <section>
                 <p>donnée récupérée sur :</p>
@@ -42,6 +68,12 @@ function CodeWars() {
                     https://www.codewars.com/api/v1/users/julien44830/code-challenges/completed?page=page
                 </p>
             </section>
+            <video
+                className="video_codewars"
+                src="src/assets/images/codewars-video.mp4"
+                autoPlay
+                loop="true"
+            ></video>
         </>
     );
 }
