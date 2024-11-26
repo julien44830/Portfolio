8d3b2002a8a09b531a83195559931a0d9e633b86
julien44830 <deniaudjulien0@gmail.com>
2024-11-27 00:05:20 +0100

    delete context && 3D animation for the loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import "./styles/index.css";
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
-import IsLoadingContext from "./contexts/HomeLoadingContext";
 
 function App() {
-    const [loading, setLoading] = useState(true);
-
-
     return (
         <>
-            <IsLoadingContext.Provider
-                value={{
-                    loading: loading,
-                    setLoading: setLoading,
-                }}
-            >
-                <Outlet />
-            </IsLoadingContext.Provider>
+            <Outlet />
         </>
     );
 }
--- src/contexts/HomeLoadingContext.jsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const IsLoadingContext = createContext();
-
-export default IsLoadingContext;
